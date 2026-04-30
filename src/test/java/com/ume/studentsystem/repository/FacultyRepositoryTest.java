8e2151aad5025ad1faf19c73f0e437a1238cf151
Vichika <vichekafc07@gmail.com>
2026-04-30 16:20:37 +0700

    unit test

--- src/test/java/com/ume/studentsystem/repository/FacultyRepositoryTest.java
+++ src/test/java/com/ume/studentsystem/repository/FacultyRepositoryTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @DataJpaTest
 public class FacultyRepositoryTest {
 
@@ -14,6 +17,13 @@ public class FacultyRepositoryTest {
     @Test
     void shouldSaveFaculty(){
         var faculty = new Faculty();
+        faculty.setName("Science and Technology");
+
+        var saved = facultyRepository.save(faculty);
+
+        assertNotNull(saved.getId());
+        assertEquals("Science and Technology", saved.getName());
+
     }
 
 }
