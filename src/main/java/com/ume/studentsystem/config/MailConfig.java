9b1b84f612b451d8a28d2e9be8298b4e4db06088
Vichika <vichekafc07@gmail.com>
2026-05-08 09:58:40 +0700

    add mail config

---
+++ src/main/java/com/ume/studentsystem/config/MailConfig.java
@@ -0,0 +1,35 @@
+package com.ume.studentsystem.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+@ConfigurationProperties(prefix = "spring.mail")
+@Data
+public class MailConfig {
+
+    private String username;
+    private String password;
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        return mailSender;
+    }
+}
