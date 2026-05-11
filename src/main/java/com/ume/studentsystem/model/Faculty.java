3af5e1c91e7428dac1b8a59dd04c2411a6fbbef5
Vichika <vichekafc07@gmail.com>
2026-05-11 15:03:27 +0700

    migration

--- src/main/java/com/ume/studentsystem/model/Faculty.java
+++ src/main/java/com/ume/studentsystem/model/Faculty.java
@@ -1,5 +1,6 @@
 package com.ume.studentsystem.model;
 
+import com.ume.studentsystem.model.audit.AuditEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -9,7 +10,7 @@ import lombok.*;
 @Getter
 @Setter
 @Table(name = "faculties")
-public class Faculty {
+public class Faculty extends AuditEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Byte id;
